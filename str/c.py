fc58dc500ab4d9c7ae8903b0a6683e984961672b
sikuatova <a_sikuatova@kbtu.kz>
2023-01-27 05:28:10 +0600

    firstcommit

--- str/c.py
+++ str/c.py
@@ -2,4 +2,11 @@ b="""Lorem ipsum dolor sit amet,
 consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
+
+#or we can use this one
+#b='''Lorem ipsum dolor sit amet,
+#consectetur adipiscing elit,
+#sed do eiusmod tempor incididunt
+#ut labore et dolore magna aliqua.'''
+
 print(b)
