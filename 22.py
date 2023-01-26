9ab80fa72a401b62479e47981bb94c2cb6fd1d02
sikuatova <a_sikuatova@kbtu.kz>
2023-01-27 05:08:47 +0600

    secondcommit

--- 22.py
+++ 22.py
@@ -1,5 +1,30 @@
-def myfunc():
-    global g
-    g="well"
-
-myfunc()
+a = "Hello World"	#str	
+b = 20	#int
+c=20.5 #float	
+d = 1j	#complex	
+e = ["apple", "banana", "cherry"]	#list	
+f = ("apple", "banana", "cherry")	#tuple	
+g = range(6)	#range	
+h = {"name" : "John", "age" : 36}	#dict	
+i = {"apple", "banana", "cherry"}	#set	
+j = frozenset({"apple", "banana", "cherry"})	#frozenset	
+k = True	#bool	
+l = b"Hello"	#bytes	
+m = bytearray(5)	#bytearray	
+n = memoryview(bytes(5))	#memoryview	
+o = None     #NoneType
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
+print(type(n))
+print(type(0))
