06fa3209614665063cd8c6796d668a250db2c45d
sikuatova <a_sikuatova@kbtu.kz>
2023-01-27 09:19:23 +0600

    firstcommit

---
+++ str/esc str/j.py
@@ -0,0 +1,12 @@
+x="Hello World"
+print(len(x))
+a=x[0]
+print(a)
+print(x[2:5])
+print(x.strip())
+print(x.upper())
+print(x.lower())
+print(x.replace("H","J"))
+b=5
+f="There are {} oranges"
+print(f.format(b))
