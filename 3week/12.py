7a435f3e88f848ccbc486ce0e2e75ec2f1381929
sikuatova <a_sikuatova@kbtu.kz>
2023-02-10 16:39:50 +0600

    firstcommit

---
+++ 3week/12.py
@@ -0,0 +1,10 @@
+def histogram(list):
+    histo = ""
+    for i in range(len(list)):
+        if i == 0:
+            histo += "*"*list[i]
+        else:
+            histo += "\n" + "*"*list[i]
+    print(histo) 
+    
+histogram([4, 9, 7])
