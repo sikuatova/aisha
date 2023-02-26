97ecac18be79a1fe540cf7a15753e0ba2f69f4f8
sikuatova <a_sikuatova@kbtu.kz>
2023-02-27 00:44:54 +0600

    firstcommit

---
+++ 4week/json/1.py
@@ -0,0 +1,16 @@
+import json
+
+jsondata = open('sample-data.json').read()
+
+json_object = json.loads(jsondata)
+print(
+    "=======================================================================================" "\n"
+    "DN                                                 Description           Speed    MTU" "\n" 
+    "-------------------------------------------------- --------------------  ------  ------")
+imdata = json_object["imdata"]
+for i in imdata:
+        dn = i["l1PhysIf"]["attributes"]["dn"]
+        descr = i["l1PhysIf"]["attributes"]["descr"]
+        speed = i["l1PhysIf"]["attributes"]["speed"]
+        mtu = i["l1PhysIf"]["attributes"]["mtu"]
+        print("{0:50} {1:20} {2:7} {3:6}".format(dn,descr,speed,mtu))
