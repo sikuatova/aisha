ac400c654c77b1844f2e62949e79f8bd44f640e2
sikuatova <a_sikuatova@kbtu.kz>
2023-03-04 18:02:39 +0600

    firstcommit

---
+++ 6week/builtin/1.py
@@ -0,0 +1,19 @@
+
+
+import math
+mylist=[1,2,3,4,5,5]
+
+x=math.prod(mylist)
+print(x)
+
+
+""" 
+def multiplyList(myList) :
+   
+    result = 1
+    for i in range(0,len(myList)):
+        result = result * myList[i]
+    return result
+    
+print(multiplyList(mylist))
+"""
