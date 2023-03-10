f41814e9058ae479738dba8b0647705abb389db2
sikuatova <a_sikuatova@kbtu.kz>
2023-03-10 21:42:08 +0600

    secondcommit

--- 6week/dir/3.py
+++ 6week/dir/3.py
@@ -4,4 +4,10 @@ print("Test a path exists or not:")
 path = r"C:\Users\Айша\Desktop\1week\6week\dir\b.txt"
 check = os.path.exists(path)
 print(check)
+if check:
+    print("\nFile name of the path:")
+    print(os.path.basename(path))
+
+    print("\nDir name of the path:")
+    print(os.path.dirname(path))
 
